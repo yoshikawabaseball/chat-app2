cc41e1d9d88bfccf0a9fb166e5ca7ce381a89bc5
yoshikawabaseball <zhixingt9@gmail.com>
2020-08-11 15:54:41 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
+  
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
